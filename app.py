34563d9398138bd979b5ed7074be6356084cc605
Cao Tri <caotri.do@engie.com>
2021-08-13 11:16:41 +0200

    ADD : 1st commit

---
+++ app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 13 10:23:34 2021
+
+@author: VG6196
+"""
+
+from flask import Flask, request, redirect, url_for, flash, jsonify
+import numpy as np
+import pickle as p
+import json
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/api/predict', methods=['POST'])
+def makecalc():
+    data = request.get_json()
+    # print(data)
+    # prediction = np.array2string(model.predict(data))
+    data_json=json.loads(data)
+    df=pd.DataFrame(data_json)
+    
+    multiplier = float(request.headers.get('multiplier'))
+    
+    df["Vente Simulée"] = df['Vente'].apply(lambda x: x*multiplier)
+    prediction = df.to_dict(orient="records")
+    # prediction = [0, 1] ; # data.copy()
+    # return jsonify(prediction)
+    return json.dumps(prediction)
+
+if __name__ == '__main__':
+    # modelfile = 'models/final_prediction.pickle'
+    # model = p.load(open(modelfile, 'rb'))
+    app.run(debug=True, host='0.0.0.0')
---
+++ test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 13 10:32:06 2021
+
+@author: VG6196
+"""
+
+#%% Importing the requests library
+import requests
+import json
+import pandas as pd
+
+
+#%% Create the dataset
+lst_pays = ["France", "UK", "US"]
+lst_vente = [10, 20, 30]
+df_data = pd.DataFrame()
+df_data["Pays"] = lst_pays
+df_data["Vente"] = lst_vente
+
+
+#%% API Call
+url = "http://localhost:5000/api/predict"
+data = df_data.to_dict(orient="records")
+headers = {
+            "multiplier": "-1"
+            }
+r = requests.post(url, json=json.dumps(data), headers = headers)
+
+#%% API Return
+json_data = json.loads(r.text)
+print(json_data)
