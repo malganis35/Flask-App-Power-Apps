d080d2955297da148e2c78ff84a32812bd6062b6
Cao Tri <caotri.do@engie.com>
2021-08-13 11:47:43 +0200

    RENAME : wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -40,5 +40,6 @@ if __name__ == '__main__':
     # model = p.load(open(modelfile, 'rb'))
     # app.run(debug=True, host='0.0.0.0')
     
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(debug=True, host='0.0.0.0', port=port)
+    app.run()
